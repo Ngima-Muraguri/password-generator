efa6d5cb18f777b1844dd2e42cb6fa101e770c21
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-04-25 23:58:28 +0300

    added a test class

--- password_test.py
+++ password_test.py
@@ -1,2 +1,9 @@
 import unittest #import unittestmodule
 from user import User #import the user class
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        '''
+        method to run before each test
+        '''
+        self.new_user=User("MilcahMuraguri","#Youcannotfind5") #new User created
