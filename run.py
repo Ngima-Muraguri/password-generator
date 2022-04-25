38885ac0112c54e3ee6bf9f7bcfe605ab3f4ac37
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-04-26 00:37:00 +0300

    added search method

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ def create_useraccount(username, password):
     '''
     new_user = User(username, password)
     return new_user
-    
+
 ### save the user account
 def save_user(user):
     '''
@@ -26,3 +26,48 @@ def save_credentials(credentials):
     '''
 
     credentials.save_credentials
+
+    ### find user using username
+
+def find_user(username):
+    '''
+    method for find user using username
+    '''
+    return User.find_user(username)
+
+         # create credentials
+def create_credentials(account, email, passcode):
+    '''
+    method credentials details
+    '''
+    new_credentials = Credentials(account, email, passcode)
+    return new_credentials
+
+####save_credentials
+def save_credentials(credentials):
+    '''
+    save credentials
+    '''
+    credentials.save_credentials()
+
+##Display all the saved credentials
+def display_credentials():
+    '''
+    method to display all the saved credentials
+    '''
+    return Credentials.display_credentials()
+
+    #search for the account credentials
+
+def find_account(account):
+    '''
+    method to search for an account
+    '''
+    return Credentials.find_account(account)
+    
+##delete the account credentials
+def delete_credentials(account):
+    '''
+    method to delete account
+    '''
+    account.delete_credentials()
