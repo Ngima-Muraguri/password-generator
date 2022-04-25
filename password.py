5a965849608de0e2c6b508cd5f2f8105e3b7bc01
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-04-25 12:20:30 +0300

    gave properties to class

--- password.py
+++ password.py
@@ -3,4 +3,18 @@ import pyperclip
 
 class user:
     new_account = []
-    
+
+
+    def _init_(self,first_name,second_name,email,password):
+        '''
+        Args:
+            first_name:user first name.
+            second_name :user second name.
+            email:user email.
+            password:user password
+        '''
+
+        self.first_name = first_name
+        self.second_name = second_name
+        self.email = email
+        self.password = password
