86820458cdc69db62fe7f57edf9a11f53b5b7946
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-04-25 23:55:45 +0300

    created a class method

--- password.py
+++ password.py
@@ -7,7 +7,7 @@ class User:
         self.username = username
         self.password =password
 
-        
+
          ##save users
     def save_user(self):
         User.user_list.append(self)
@@ -16,3 +16,12 @@ class User:
     #delete user
     def delete_user(self):
         User.user_list.remove(self)
+
+    @classmethod
+    def find_user(cls, username):
+        '''
+        find username using search terms
+        '''
+        for user in cls.user_list:
+            if user.username == username:
+                return  user
