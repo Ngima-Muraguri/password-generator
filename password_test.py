2ed6b6002eaf8005d1b4ca3117ffd9f572769d25
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-04-26 00:00:41 +0300

    added second test

--- password_test.py
+++ password_test.py
@@ -7,3 +7,18 @@ class TestUser(unittest.TestCase):
         method to run before each test
         '''
         self.new_user=User("MilcahMuraguri","#Youcannotfind5") #new User created
+
+    def test__init(self):
+       
+        self.assertEqual(self.new_user.username,"MilcahMuraguri")
+        self.assertEqual(self.new_user.password,"#Youcannotfind5")
+
+        
+        ##second test
+    def test_save_user(self):
+        '''
+        check whether the user information can be saved 
+        in the user list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list), 1)
