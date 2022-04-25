8fa8b8ba31d052f1a3042b250eaa4ad5225f98b6
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-04-25 23:54:18 +0300

    created a delete user method

--- password.py
+++ password.py
@@ -6,6 +6,13 @@ class User:
     def __init__(self,username,password):
         self.username = username
         self.password =password
+
+        
          ##save users
     def save_user(self):
         User.user_list.append(self)
+
+
+    #delete user
+    def delete_user(self):
+        User.user_list.remove(self)
